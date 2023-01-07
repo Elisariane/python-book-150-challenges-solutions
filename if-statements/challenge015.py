db986b1645b922d6c88098e9c71ba0788f24c34e
Elisariane <elisarianeBS@gmail.com>
2023-01-07 09:27:27 -0300

    :sparkles: feat:  Adicionando exercícios sobre condicionais

---
+++ if-statements/challenge015.py
@@ -0,0 +1,9 @@
+# 015 Ask the user to enter their favourite colour. If they enter “red”, “RED” or “Red” display the message
+# “I like red too”, otherwise display the message “I don’t like [colour], I prefer red”.
+
+colour = input("Enter your favourite colour: \n")
+
+if colour == "RED" or colour == "Red" or colour == "red":
+    print("I like red too")
+else:
+    print("I don’t like ", colour, " I prefer red")
